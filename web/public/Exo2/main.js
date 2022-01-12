572ddb3d232bb8b1ddf7633724f54520141399c0
Jean-Pierre Duchesneau <jpduchesneau@csfoy.ca>
2022-01-12 10:47:42 -0500

    Modif importantes

--- web/public/Exo2/main.js
+++
@@ -1,8 +0,0 @@
-var button = document.querySelector(".updateColor");
-
-var divColorBlue = document.querySelector(".backgroundColorBlue")
-
-button.addEventListener("click",function(){
-    divColorBlue.classList.remove("backgroundColorBlue");
-    divColorBlue.classList.add("backgroundColorGreen");
-})
--- web/public/Exo3/common/menu.php
+++
@@ -1,9 +0,0 @@
-<nav>
-      <ul>
-          <li><a href="index.php">Accueil</a></li>
-          <li><a href="exo1.php">Exercice 1 - Table multiplaction</a></li>
-          <li><a href="exo2.php">Exercice 2 - Pyramide</a></li>
-          <li><a href="exo3.php">Exercice 3 - Cercle</a></li>
-          <li><a href="exo8.php">Exercice 8 - Personnage</a></li>
-      </ul>  
-    </nav>
